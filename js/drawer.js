85d3a8b03cdd515d160123b9ac02c8cb49ad7950
Murapyon3286 <82795887+Murapyon3286@users.noreply.github.com>
2023-12-18 23:31:42 +0900

    js追加

---
+++ js/drawer.js
@@ -0,0 +1,17 @@
+"use strict";
+// const drawer = document.querySelectorAll(".icon-block");
+
+// function toggle() {
+//   const content = this.nextElementSibling;
+//   this.classList.toggle("is-active");
+//   content.classList.toggle("is-open");
+// }
+
+// for (let i = 0; i < drawer.length; i++) {
+//   drawer[i].addEventListener("click", toggle)
+// }
+
+// 以下jQuery
+$(".icon-block").on("click", function () {
+  $(this).next().slideToggle();
+});
