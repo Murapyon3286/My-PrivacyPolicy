1db43207ec7de6ce4103b139e75cc4ac1ee00a38
Murapyon3286 <82795887+Murapyon3286@users.noreply.github.com>
2023-12-21 23:12:44 +0900

    完成

---
+++ js/form.js
@@ -0,0 +1,15 @@
+"use strict";
+function clearDate() {
+  let dateValue = document.getElementById("dateForm");
+  dateValue.value = "";
+}
+
+function clearName() {
+  let nameValue = document.getElementById("floatingInput");
+  nameValue.value = "";
+}
+
+function clearText() {
+  let textValue = document.getElementById("textInput");
+  textValue.value = "";
+}
